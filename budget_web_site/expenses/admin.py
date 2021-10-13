6c1ea9596650822a0c0b6f4a096dc1ec3277dfd6
PJankowskii <piotrjandevmail@gmail.com>
2021-10-13 12:35:12 +0200

    Some customisation for Django admin.

--- budget_web_site/expenses/admin.py
+++ budget_web_site/expenses/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import Expense, Category
 
+
 # Register your models here.
-admin.site.register(Expense)
-admin.site.register(Category)
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('id', 'amount', 'owner', 'category', 'date')
+    list_per_page = 20
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+
+admin.site.register(Expense, ExpenseAdmin)
+admin.site.register(Category, CategoryAdmin)
--- budget_web_site/incomes/admin.py
+++ budget_web_site/incomes/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import Income, Source
 
+
 # Register your models here.
-admin.site.register(Income)
-admin.site.register(Source)
+class IncomeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'amount', 'owner', 'source', 'date')
+    list_per_page = 20
+
+
+class SourceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+
+admin.site.register(Income, IncomeAdmin)
+admin.site.register(Source, SourceAdmin)
